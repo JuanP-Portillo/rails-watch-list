a42026077141d182a3d00d6ef1cc536e7bfdb3c9
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-03-09 17:02:23 -0600

    only missing the html's

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
