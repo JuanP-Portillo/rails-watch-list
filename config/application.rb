bce8a41723bdbb0b7045a47dc7ddaec39c6d7912
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-03-10 10:26:46 -0600

    teste

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module RailsWatchList
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
+    config.assets.initialize_on_precompile = false
+
     config.generators.system_tests = nil
   end
 end
